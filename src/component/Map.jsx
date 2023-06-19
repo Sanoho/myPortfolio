199cfee2595177795b1828102daa42ed631b26cf
Timothy <sanohoang@gmail.com>
2023-06-19 14:22:26 -0500

    updated map

--- src/component/Map.jsx
+++ src/component/Map.jsx
@@ -6,6 +6,7 @@ import {
   Geography,
   Marker,
   Annotation,
+  ZoomableGroup,
 } from "react-simple-maps";
 
 import allStates from "../data/allstates.json";
@@ -33,59 +34,67 @@ const Map = () => {
       }}
       style={{ width: "100%", height: "100%" }}
     >
-      <Geographies geography={geoUrl}>
-        {({ geographies }) => (
-          <>
-            {geographies.map((geo) => (
-              <Geography
-                key={geo.rsmKey}
-                stroke="#FFF"
-                strokeWidth={0.5}
-                geography={geo}
-                fill="#2c065d17"
-              />
-            ))}
-            {geographies.map((geo) => {
-              const centroid = geoCentroid(geo);
-              const cur = allStates.find((s) => s.val === geo.id);
-              return (
-                <g key={geo.rsmKey + "-name"}>
-                  {cur &&
-                    centroid[0] > -160 &&
-                    centroid[0] < -67 &&
-                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
-                      <Marker coordinates={centroid}>
-                        <text
-                          y="2"
-                          fontSize={14}
-                          textAnchor="middle"
-                          stroke="white"
+      <ZoomableGroup center={[-94.346, 36.9828]}>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) => (
+            <>
+              {geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  stroke="#FFF"
+                  strokeWidth={0.5}
+                  geography={geo}
+                  fill="#2c065d17"
+                />
+              ))}
+              {geographies.map((geo) => {
+                const centroid = geoCentroid(geo);
+                const cur = allStates.find((s) => s.val === geo.id);
+                return (
+                  <g key={geo.rsmKey + "-name"}>
+                    {cur &&
+                      centroid[0] > -160 &&
+                      centroid[0] < -67 &&
+                      (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                        <Marker coordinates={centroid}>
+                          <text
+                            y="2"
+                            fontSize={14}
+                            textAnchor="middle"
+                            stroke="white"
+                          >
+                            {cur.id}
+                          </text>
+                        </Marker>
+                      ) : (
+                        <Annotation
+                          subject={centroid}
+                          dx={offsets[cur.id][0]}
+                          dy={offsets[cur.id][1]}
                         >
-                          {cur.id}
-                        </text>
-                      </Marker>
-                    ) : (
-                      <Annotation
-                        subject={centroid}
-                        dx={offsets[cur.id][0]}
-                        dy={offsets[cur.id][1]}
-                      >
-                        <text
-                          x={4}
-                          fontSize={14}
-                          alignmentBaseline="middle"
-                          stroke="white"
-                        >
-                          {cur.id}
-                        </text>
-                      </Annotation>
-                    ))}
-                </g>
-              );
-            })}
-          </>
-        )}
-      </Geographies>
+                          <text
+                            x={4}
+                            fontSize={14}
+                            alignmentBaseline="middle"
+                            stroke="white"
+                          >
+                            {cur.id}
+                          </text>
+                        </Annotation>
+                      ))}
+                  </g>
+                );
+              })}
+            </>
+          )}
+        </Geographies>
+        <Marker coordinates={[-94.346, 36.9828]}>
+          <circle r={6} fill="#da4ea2" />
+          <text fill="white" fontWeight="bold" textAnchor="start">
+            Joplin
+          </text>
+        </Marker>
+      </ZoomableGroup>
     </ComposableMap>
   );
 };
